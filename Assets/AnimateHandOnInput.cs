e0cd924cc4b41666e91f54ae68cec6467af05dfb
amit800IL <108745954+amit800IL@users.noreply.github.com>
2024-01-30 20:09:18 +0200

    initial xr input

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -1,8 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
-    
+    public InputActionProperty picnchAnimationAction;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float triggerValue = picnchAnimationAction.action.ReadValue<float>();
+
+        Debug.Log(triggerValue);
+    }
 }
