a658a018980abaedf59ae707b392e353bea2435a
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2024-02-13 20:51:12 +0200

    fire bullet event

---
+++ Assets/Scripts/FireProjectileOnActivate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireProjectileOnActivate : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float fireSpeed = 10;
+    void Start()
+    {
+        XRGrabInteractable grabable= GetComponent<XRGrabInteractable>();
+        grabable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet , spawnPoint.position, spawnPoint.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnedBullet.transform.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+    }
+}
